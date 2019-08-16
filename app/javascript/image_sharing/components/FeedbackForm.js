db4a8003a43f983c1942f5daeb12d23e09769e8c
Jiayou Sun <jiayou.sun@appfolio.com>
2019-08-16 11:02:40 -0700

    Add FeedbackForm

--- app/javascript/image_sharing/components/FeedbackForm.js
+++ app/javascript/image_sharing/components/FeedbackForm.js
@@ -9,11 +9,11 @@ class FeedbackForm extends React.Component {
       <Form className="offset-md-3 col-md-6" style={{ width: `${50}%` }}>
         <FormGroup>
           <Label>Your name:</Label>
-          <Input type="text" value={this.props.store.feedback_name} onChange={event => this.props.store.setName(event.target.value)} />
+          <Input type="text" value={this.props.store.feedbackName} onChange={event => this.props.store.setName(event.target.value)} />
         </FormGroup>
         <FormGroup>
           <Label>Comments</Label>
-          <Input type="textarea" rows="10" value={this.props.store.feedback_text} onChange={event => this.props.store.setText(event.target.value)} />
+          <Input type="textarea" rows="10" value={this.props.store.feedbackText} onChange={event => this.props.store.setText(event.target.value)} />
         </FormGroup>
         <Button value="Submit" color="primary">Submit</Button>
       </Form>
